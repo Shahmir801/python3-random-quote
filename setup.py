6c478fb2c435bbcfde0311a41b1c598fff1c00da
Shahmir-Ali <72006925+Shahmir-Ali@users.noreply.github.com>
2020-12-08 04:26:26 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
-print("Hello")
+from setuptools import setup, find_packages
+
+setup(
+    name='randomquote',
+    version='0.1',
+    description='Get a random quote',
+    url='http://github.com/trstringer/python3-random-quote',
+    author='Thomas Stringer',
+    author_email='mian.shahmir.ali@gmail.com',
+    license='MIT',
+    install_requires=['requests'],
+    packages=find_packages()
+    #entry_points=dict(
+        #console_scripts=['rq=src.main:display_quote']
+    #)
+)
